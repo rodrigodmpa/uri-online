00e9b39f4db63bb0a815217b763fe6d1bfdb6e52
Rodrigo Demetrio <rodrigo.demetrio.palma@gmail.com>
2019-04-10 20:00:09 -0300

    solving 1218

--- C++/1218.cpp
+++ C++/1218.cpp
@@ -1,36 +1,62 @@
 #include <iostream>
- 
+#include <string>
+
 using namespace std;
- 
+
 class Sapatos 
 {   
     // Atributos públicos da classe
     public: 
-    int tamanho; 
-    char genero;
+    string procura,encontra;
+	int qnt_fem,qnt_masc;
     
-    // Construtor parametrizado para que possamos inicializar a classe com os atributos tamanho e genero
-    Sapatos(int a,char b)
+    /* Construtor parametrizado para que possamos inicializar a classe com os atributos, o sapato que procuro e o que encontro
+	*  E as quantidades com zero.
+	*/
+    Sapatos(string a ,string b)
     {   
-        tamanho=a;
-        genero=b;
+        procura = a;
+        encontra = b;
+		qnt_fem = 0;
+		qnt_masc = 0;
     } 
 
-    // Metodo que printa na tela o vencedor
-    void ganhador() 
+    // Metodo que calcula a quantidade de sapatos que tenho interesse
+    void calcula() 
     {   
+		for(int i = 0; i < procura.length(); i += 5){
+    		if(procura[i] == encontra[0] && procura[i+1] == encontra[1]){
+    			if(procura[i+3] == 'M')
+    				qnt_masc++;
+    			else
+    				qnt_fem++;
+    		}
+    	}
     }
 }; 
 
 int main() {
- 
-    int x,y,caracter;
-    cin >> caracter;
-    while(caracter) != EOF){
-        cin >> x >> y;
-        // Instancia da classe funcao com os parametros que o construtor precisa
-        Funcoes func(x,y);
-        func.ganhador();
+	string procura, encontra;
+    int caso = 1;
+    
+
+    while(getline(cin, procura)){
+    	
+    	getline(cin, encontra);
+    	
+		Sapatos sapato(encontra,procura);
+
+		sapato.calcula();
+
+    	if(caso != 1)
+    		cout << endl;
+		
+    	cout << "Caso "<< caso <<":" << endl;
+    	cout << "Pares Iguais: " << sapato.qnt_masc + sapato.qnt_fem << endl;
+    	cout << "F: " << sapato.qnt_fem << endl;
+    	cout << "M: " << sapato.qnt_masc << endl;
+
+    	caso++;
     }
 
     return 0;
